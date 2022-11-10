d4d40089e479a4f9ff16491b2432d042c2fc7f0d
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-11-10 17:19:59 +0100

    completato il progetto mail

--- mail/js/myscript.js
+++ mail/js/myscript.js
@@ -6,11 +6,15 @@
 // creo un array contenente l'email dei soggetti invitati
 const invitatiEmailList = ["invitato1@festa.it", "invitato2@festa.it", "invitato3@festa.it", "invitato4@festa.it", "invitato5@festa.it", "invitato6@festa.it"];
 
+console.log(invitatiEmailList);
+
 // creo il prompt per far inserire all'user la sua email
 const emailUser = prompt("Inserisci la tua email, così controlleremo se fai parte degli invitati");
 
+console.log(emailUser);
+
 // creo la variabile per confermare la presenza dell'email all'interno della lista
-let seiPresente;
+let seiPresente = false;
 
 // creo il ciclo per controllare la presenza dell'email inserita dall'user nella lista degli invitati
 
@@ -20,11 +24,10 @@ for (let i = 0; i < invitatiEmailList.length; i ++) {
 
         seiPresente = true;
 
-    } else if (emailUser !== invitatiEmailList[i]) {
+        console.log(seiPresente);
+    } 
 
-        seiPresente = false;
-
-    }
+    console.log(i);
 
 }
 
