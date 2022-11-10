b32fb16f38611855c14c2f828d9a95d25481936d
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-11-10 14:54:54 +0100

    Creazione ciclo per controllare la presenza dell'email

---
+++ mail/js/myscript.js
@@ -0,0 +1,28 @@
+// Mail:
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+
+// creo un array contenente l'email dei soggetti invitati
+const invitatiEmailList = ["invitato1@festa.it", "invitato2@festa.it", "invitato3@festa.it", "invitato4@festa.it", "invitato5@festa.it"];
+
+// creo il prompt per far inserire all'user la sua email
+let emailUser = prompt("Inserisci la tua email, così controlleremo se fai parte degli invitati");
+
+// creo il ciclo per controllare la presenza dell'email inserita dall'user nella lista degli invitati
+
+for (let i = 0; i < invitatiEmailList.length; i ++) {
+
+    if (emailUser == invitatiEmailList[i]) {
+
+        console.log("Fai parte degli invitati alla festa.");
+
+    } else if (emailUser != invitatiEmailList[i]) {
+
+        console.log("Non fai parte degli invitati alla festa.");
+
+    }
+
+}
